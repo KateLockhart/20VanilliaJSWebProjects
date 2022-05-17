c7ab0e0ce8ec8a7a0819b46bcfc04089e296126e
KateLockhart <katelockhart707@gmail.com>
2022-05-16 20:08:47 -0400

    Progressing through script.js for 05_DOMArrayMethods.

--- 05_DOMArrayMethods/script.js
+++ 05_DOMArrayMethods/script.js
@@ -29,5 +29,28 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 };
 
+// Update DOM
+// ES6 implemented setting a default value parameter if nothing gets passed to the function, if nothing is passed the function will use the array of variable data from our file
+function updateDOM(providedData = data) {
+    // Clear the main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money - resource found by Traversy => https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
