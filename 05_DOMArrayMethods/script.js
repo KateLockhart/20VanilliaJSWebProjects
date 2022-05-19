cc3aa5cd6024a08c43ba82c64f2ae112ef3b6141
KateLockhart <katelockhart707@gmail.com>
2022-05-18 21:23:43 -0400

    Completed O5_DOMArrayMethods. Created file structure and finished HTML for O6_ModalMenuSlider.

--- 05_DOMArrayMethods/script.js
+++ 05_DOMArrayMethods/script.js
@@ -26,6 +26,38 @@ async function getRandomUser() {
     addData(newUser);
 };
 
+// Double everyone's money, .map()
+function doubleMoney() {
+    data = data.map((user) => {
+        return { ...user, money: user.money * 2 };
+    });
+
+    updateDOM();
+};
+
+// Sort users by richest, .sort()
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+};
+
+// Filter only millionaires, .filter()
+function showMillionaires() {
+    data = data.filter(user => user.money >= 1000000);
+
+    updateDOM();
+};
+
+// Calculate the total wealth, .reduce()
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -33,7 +65,7 @@ function addData(obj) {
     updateDOM();
 };
 
-// Update DOM
+// Update DOM, .forEach()
 // ES6 implemented setting a default value parameter if nothing gets passed to the function, if nothing is passed the function will use the array of variable data from our file
 function updateDOM(providedData = data) {
     // Clear the main div
@@ -54,3 +86,7 @@ function formatMoney(number) {
 
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
