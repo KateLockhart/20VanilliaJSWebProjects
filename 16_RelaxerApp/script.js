5e4b579215c369ee01e047e649492c3da7350c60
KateLockhart <katelockhart707@gmail.com>
2022-06-07 15:10:22 -0400

    16_RelaxerApp project finished, completed scripts needed for animations, removed placeholder text from HTML.

---
+++ 16_RelaxerApp/script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+// console.log(breatheTime, holdTime);
+
+breathAnimation();
+
+// Changing the timing, text, and classes
+function breathAnimation() {
+  text.innerText = "Breathe In";
+  container.className = "container grow";
+
+  setTimeout(() => {
+    text.innerText = "Hold";
+
+    setTimeout(() => {
+      text.innerText = "Breathe Out";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
+}
+
+// Set interval allows us to loop the animation
+setInterval(breathAnimation, totalTime);
